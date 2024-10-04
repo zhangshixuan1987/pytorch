04502681292a99dbccc7022662b0e1777bc82b2a
Xinran / Allan Rui <arui@meta.com>
2024-10-04 11:09:11 -0700

    [WIP] Add dtype attribute to TritonCSEVariable (#136778)
    Summary:
    
    
    - This diff introduces `dtype` attribute to `TritonCSEVariable` and a dtype propagation helper function to infer dtype from input to output for each op.
    
    - In the future, we could use this to do dtype-aware codegen
    
    Test Plan: CI
    
    Differential Revision: D61815079

--- torch/_inductor/codegen/common.py
+++ torch/_inductor/codegen/common.py
@@ -1480,11 +1480,17 @@ class CSEVariable:
     See example of TritonCSEVariable in triton.py
     """
 
-    def __init__(self, name, bounds: ValueRanges[Any]):
+    def __init__(
+        self,
+        name,
+        bounds: ValueRanges[Any],
+        dtype: Optional[torch.dtype] = None,
+    ):
         assert isinstance(bounds, ValueRanges)
         self.name = name
         self.bounds = bounds
         self.use_count = 1  # track how many tims this expression is used
+        self.dtype = dtype
 
     def __str__(self):
         return self.name
@@ -1563,6 +1569,7 @@ class CSE:
         buffer: IndentedBuffer,
         expr: Union[str, CSEVariable, OpsValue, IndentedBuffer],
         *,
+        dtype: Optional[torch.dtype] = None,
         bounds: ValueRanges[Any] = ValueRanges.unknown(),
         write=True,
         assignment=True,
@@ -1582,7 +1589,7 @@ class CSE:
         cache_key = expr.getvalue() if isinstance(expr, IndentedBuffer) else expr
         var = self.cache.get(cache_key, None)
         if not var:
-            var = self.newvar(bounds)
+            var = self.newvar(dtype, bounds)
             self.cache[cache_key] = var
             if write:
                 if V.kernel.current_node:
@@ -1606,11 +1613,58 @@ class CSE:
 
         return var
 
-    def newvar(self, bounds: ValueRanges[Any] = ValueRanges.unknown()) -> CSEVariable:
+    def newvar(
+        self,
+        dtype: Optional[torch.dtype] = None,
+        bounds: ValueRanges[Any] = ValueRanges.unknown(),
+    ) -> CSEVariable:
         var_name = f"{self.name_prefix}{next(self.iter_buffer_ids)}"
-        var = V.kernel.create_cse_var(var_name, bounds)
+        var = V.kernel.create_cse_var(var_name, bounds, dtype)
         self.varname_map[var_name] = var
         return var
+
+
+def propagate_dtype(op_name, *args, **kwargs):
+    """
+    Propagate dtype from args to output
+    """
+
+    if len(args) == 1 and isinstance(args[0], CSEVariable):
+        return args[0].dtype
+    elif op_name == "constant":
+        return args[1]  # args[1] is dtype
+    elif op_name == "to_dtype":
+        return args[1]  # args[1] is dtype
+    elif op_name == "to_dtype_bitcast":
+        return args[1]  # Inputs[SrcTensor, target_dtype, original_dtype]
+    elif op_name in ["isnan", "lt"]:
+        return torch.bool
+    elif op_name == "where" and isinstance(args[2], CSEVariable):
+        return args[2].dtype
+    elif op_name == "index_expr":
+        return args[1]
+    elif op_name == "masked" and isinstance(args[0], CSEVariable):
+        return args[0].dtype
+    elif op_name == "load_seed":
+        return torch.float32
+    elif op_name == "randint64":
+        return torch.int64
+
+    arg_dtypes = []
+
+    for arg in args:
+        if isinstance(args, CSEVariable):
+            arg_dtypes.append(arg.dtype)
+        elif isinstance(arg, OpsValue) and isinstance(arg.value, CSEVariable):
+            arg_dtypes.append(arg.value.dtype)
+
+    if len(arg_dtypes) == 2:
+        return torch.promote_types(*arg_dtypes)
+    elif len(arg_dtypes) == 1:
+        return arg_dtypes[0]
+    else:
+        # Fallback to default dtype of torch.float32
+        return torch.float32
 
 
 class CodeGen:
@@ -1846,8 +1900,10 @@ class Kernel(CodeGen):
                     value = getattr(parent_handler, name)(*args, **kwargs)  # type: ignore[has-type]
 
                     def do_cse(v):
+                        output_dtype = propagate_dtype(name, *args, **kwargs)
+
                         csevar = V.kernel.cse.generate(
-                            V.kernel.compute, v, bounds=bounds
+                            V.kernel.compute, v, dtype=output_dtype, bounds=bounds
                         )
                         csevar.update_on_args(name, args, kwargs)
                         return csevar
